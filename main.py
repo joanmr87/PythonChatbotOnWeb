b3b7b2afab775623f6a06ef41129bb2177b6923e
joanmr87 <joanmr87@egmail.com>
2023-07-24 16:21:43 -0300

    modify code fo Flask app

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# chatbot.py
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
 import os
 import openai
 import sys
@@ -14,23 +18,14 @@ from langchain.memory import ConversationBufferMemory
 from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 from langchain.vectorstores import DocArrayInMemorySearch
 from langchain.document_loaders import TextLoader, PyPDFLoader
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-from starlette.middleware.cors import CORSMiddleware
-import uvicorn
 
-# Set up environment
-sys.path.append('../..')
-pn.extension()
+
 load_dotenv(".env")
 
-# Load environment variables
 def load_env_vars():
     _ = load_dotenv(find_dotenv())
     openai.api_key  = os.environ['OPENAI_API_KEY']
 
-# Get model name
 def get_llm_name():
     current_date = datetime.datetime.now().date()
     if current_date < datetime.date(2023, 9, 2):
@@ -39,15 +34,18 @@ def get_llm_name():
         llm_name = "gpt-3.5-turbo"
     return llm_name
 
-# Load necessary variables
 llm_name = get_llm_name()
 print(llm_name)
 
 embedding = OpenAIEmbeddings()
 vectordb = Chroma(embedding_function=embedding)
+
+question = "What are major topics for this class?"
+docs = vectordb.similarity_search(question,k=3)
+len(docs)
+
 llm = ChatOpenAI(model_name=llm_name, temperature=0)
 
-# Define the template for the chat
 template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
 {context}
 Question: {question}
@@ -93,9 +91,6 @@ def load_db(file, chain_type, k):
         return_generated_question=True,
     )
     return qa 
-
-
-
 class cbfs(param.Parameterized):
     chat_history = param.List([])
     answer = param.String("")
@@ -122,18 +117,16 @@ class cbfs(param.Parameterized):
 
     def convchain(self, query):
         if not query:
-            return pn.WidgetBox(pn.Row('User:', pn.pane.Markdown("", width=600)), scroll=True)
+            return {'user': "", 'bot': ""}
+
         result = self.qa({"question": query, "chat_history": self.chat_history})
         self.chat_history.extend([(query, result["answer"])])
         self.db_query = result["generated_question"]
         self.db_response = result["source_documents"]
         self.answer = result['answer'] 
-        self.panels.extend([
-            pn.Row('User:', pn.pane.Markdown(query, width=600)),
-            pn.Row('ChatBot:', pn.pane.Markdown(self.answer, width=600, style={'background-color': '#F6F6F6'}))
-        ])
-        inp.value = ''  #clears loading indicator when cleared
-        return pn.WidgetBox(*self.panels,scroll=True)
+
+        return {'user': query, 'bot': self.answer}
+
 
     @param.depends('db_query ', )
     def get_lquest(self):
@@ -170,69 +163,32 @@ class cbfs(param.Parameterized):
         return
 
 
+load_dotenv(".env")
 
+cb = cbfs()  # Crear una instancia de tu clase cbfs
 
+app = Flask(__name__)
 
-# Create a chatbot
-cb = cbfs()
+@app.route('/api/convchain', methods=['POST'])
+def convchain():
+    data = request.get_json()
+    query = data.get('query')
+    if not query:
+        return jsonify({'error': 'No query provided'}), 400
+    result = cb.convchain(query)
+    print(result)
+    return jsonify(result)
 
-file_input = pn.widgets.FileInput(accept='.pdf')
-button_load = pn.widgets.Button(name="Load DB", button_type='primary')
-button_clearhistory = pn.widgets.Button(name="Clear History", button_type='warning')
-button_clearhistory.on_click(cb.clr_history)
-inp = pn.widgets.TextInput( placeholder='Enter text here…')
+@app.route('/api/load_db', methods=['POST'])
+def load_db():
+    data = request.get_json()
+    file = data.get('file')
+    chain_type = data.get('chain_type')
+    k = data.get('k')
+    if not all([file, chain_type, k]):
+        return jsonify({'error': 'Missing parameters'}), 400
+    result = cb.load_db(file, chain_type, k)
+    return jsonify(result)
 
-bound_button_load = pn.bind(cb.call_load_db, button_load.param.clicks)
-conversation = pn.bind(cb.convchain, inp) 
-
-jpg_pane = pn.pane.Image( './img/convchain.jpg')
-
-tab1 = pn.Column(
-    pn.Row(inp),
-    pn.layout.Divider(),
-    pn.panel(conversation,  loading_indicator=True, height=300),
-    pn.layout.Divider(),
-)
-tab2= pn.Column(
-    pn.panel(cb.get_lquest),
-    pn.layout.Divider(),
-    pn.panel(cb.get_sources ),
-)
-tab3= pn.Column(
-    pn.panel(cb.get_chats),
-    pn.layout.Divider(),
-)
-tab4=pn.Column(
-    pn.Row( file_input, button_load, bound_button_load),
-    pn.Row( button_clearhistory, pn.pane.Markdown("Clears chat history. Can use to start a new topic" )),
-    pn.layout.Divider(),
-    pn.Row(jpg_pane.clone(width=400))
-)
-dashboard = pn.Column(
-    pn.Row(pn.pane.Markdown('# ChatWithYourData_Bot')),
-    pn.Tabs(('Conversation', tab1), ('Database', tab2), ('Chat History', tab3),('Configure', tab4))
-)
-
-# Este es el código que añadimos para servir el panel como una aplicación web.
-if __name__ == "__main__":
-    pn.serve(dashboard)
-
-# FastAPI setup
-app = FastAPI()
-
-# Ensure all CORS are allowed (this can be configured for more security)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get('/', response_class=HTMLResponse)
-def root():
-    return pn.serve(dashboard, show=False, start=False, port=50999, websocket_origin="*")
-
-# Run the app
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=50999, log_level="info")
+if __name__ == '__main__':
+    app.run(debug=True)
