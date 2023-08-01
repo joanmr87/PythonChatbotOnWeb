1b51d5be26a993cf1e3bcab2e30ca192d916dc16
joanmr87 <joanmr87@egmail.com>
2023-08-01 14:39:18 -0300

    Improve Prompt

--- app.py
+++ app.py
@@ -48,7 +48,11 @@ len(docs)
 
 llm = ChatOpenAI(model_name=llm_name, temperature=0)
 
-template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
+template = """You are an helpful AI assistant . Use the following pieces of context to answer the question at the end.
+Answer in the same language as the question.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+
 {context}
 Question: {question}
 Helpful Answer:"""
