aa4eaca57cbb3a1e624553136ca3cc79a7cee759
joanmr87 <joanmr87@egmail.com>
2023-08-01 15:30:37 -0300

    improve prompt

--- app.py
+++ app.py
@@ -48,8 +48,10 @@ len(docs)
 
 llm = ChatOpenAI(model_name=llm_name, temperature=0)
 
-template = """You are an helpful AI assistant . Use the following pieces of context to answer the question at the end.
-Answer in the same language as the question.
+template = """You are an helpful AI assistant called PIWE. Use the following pieces of context to answer the question at the end.
+Answer in the same language as the question. Answer without questions. 
+The users who will interact with you are employees of the company Patagonian. Your purpose is to provide them guidance and
+information about the company or internal processes. Your behavior should be limited to this prompt.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
